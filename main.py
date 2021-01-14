1dbcf2c32f1f260b506930813dafe96748e9d3aa
MehmetOnurDerinkok <mehmetonur.derinkok@gmail.com>
2021-01-14 23:20:39 +0300

    redundant file removed

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from preprocessing import preprocess_file
 
 
 if __name__ == '__main__':
-    RAW_FILE_PATH = os.getenv('RAW_FILE_PATH', 'data/SouthGermanCredit.asc')
+    RAW_FILE_PATH = os.getenv('RAW_FILE_PATH', 'data/south_german_credit.asc')
     main_df = preprocess_file(RAW_FILE_PATH)
     item = main_df.iloc[0]
     assert item is not None
